1a8c0bf51074b2daba8e2faece189dea84071efc
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-08-30 12:57:46 +0200

    Responsive Navbar and hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+"use client"
+import Hero from '@/components/Hero';
+import MobileNav from '@/components/MobileNav';
+import Navbar from '@/components/Navbar'
+import React, { useState } from 'react'
 
 const page = () => {
+  const [ nav, setNav ] = useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
   return (
-    <div>Homepage</div>
+    <div className='overflow-x-hidden'>
+      <div>
+        {/* Navbar */}
+        <MobileNav nav={nav} closeNav={closeNav} /> 
+        <Navbar openNav={openNav} />
+        {/* Hero Section */}
+        <Hero />
+      </div>
+    </div>
   )
 }
 
---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Particle from './Particle'
+
+const Hero = () => {
+  return (
+    <div className='h-[88vh]'>
+        <Particle />
+    </div>
+  )
+}
+
+export default Hero
