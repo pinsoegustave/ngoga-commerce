14fb02abbad9719f316ffee501081c104c1ed5d6
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-09-01 10:51:56 +0200

    Fading and animation

--- components/About.tsx
+++ components/About.tsx
@@ -18,7 +18,7 @@ const About = () => {
                     <ArrowDownTrayIcon className='w-[1.6rem] h-[1.7rem] text-black' />
                 </button>
             </div>
-            <div className='lg:w-[500px] mx-auto md:mx-0 mt-[2rem] lg:mt-0 lg:h-[500px] w-[300px] h-[300px] relative'>
+            <div data-aos="fade-left" className='lg:w-[500px] mx-auto md:mx-0 mt-[2rem] lg:mt-0 lg:h-[500px] w-[300px] h-[300px] relative'>
                 <Image 
                 src={"/theman.webp"}
                 alt='profile'           
--- components/Services.tsx
+++ components/Services.tsx
@@ -8,7 +8,7 @@ const Services = () => {
             My <span className='text-yellow-400'>Architecture</span>
         </p>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto items-center gap-[3rem] mt-[4rem] text-white'>
-            <div>
+            <div data-aos="fade-right" data-aos-delay="500">
                 <div className='bg-red-700 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]'>
                     <HomeIcon className='w-[6rem] h-[6rem] mx-auto text-[#d3fae8]' />
                     <h1 className='text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5]'>Residential</h1>
@@ -17,7 +17,7 @@ const Services = () => {
                     </p>
                 </div>
             </div>
-            <div>
+            <div data-aos="fade-up" data-aos-delay="500">
                 <div className='bg-blue-700 hover:scale-110 transform transition-all duration-300 uppercase font-semibold text-center p-[2rem]'>
                     <BuildingLibraryIcon className='w-[6rem] h-[6rem] mx-auto text-[#d3fae8]' />
                     <h1 className='text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5]'>Commercial</h1>
@@ -26,7 +26,7 @@ const Services = () => {
                     </p>
                 </div>
             </div>
-            <div>
+            <div data-aos="fade-left" data-aos-delay="500"> 
                 <div className='bg-yellow-500 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]'>
                     <CodeBracketSquareIcon className='w-[6rem] h-[6rem] mx-auto text-[#d3fae8]' />
                     <h1 className='text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5]'>Landscape</h1>
