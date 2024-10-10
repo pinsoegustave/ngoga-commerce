b2bb08ca966c7b8d299c82e5ad3d260934763031
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-10-10 17:04:59 +0200

    Fetch API: Delete music API

--- components/admin-panel/MusicRow.tsx
+++ components/admin-panel/MusicRow.tsx
@@ -1,6 +1,9 @@
 import { IMusic } from '@/app/admin/dashboard/music/page';
+import { setLoading } from '@/redux/features/loadingSlice';
+import { useAppDispatch } from '@/redux/hooks';
+import { makeToast } from '@/utils/helper';
+import axios from 'axios';
 import { Dispatch, SetStateAction } from 'react'
-import { CiEdit } from 'react-icons/ci'
 import {  RiDeleteBin5Line } from 'react-icons/ri'
 
 interface PropsType {
@@ -11,8 +14,18 @@ interface PropsType {
 
 const MusicRow = ({ srNo, music, setOpenPopup }: PropsType) => {
 
-    const onEdit = () => {
-        setOpenPopup(true);
+    const dispatch = useAppDispatch();
+
+    const onDelete = () => {
+        dispatch(setLoading(true));
+
+        axios.delete(`/api/delete_music/${music._id}`).then(res => {
+            console.log(res.data);
+            makeToast("Music is deleted!!");
+
+        })
+        .catch((err) => console.log(err))
+        .finally(() => dispatch(setLoading(false)));
       };
     
   return (
@@ -28,7 +41,7 @@ const MusicRow = ({ srNo, music, setOpenPopup }: PropsType) => {
         </td>
         <td>
             <div className='text-2xl flex items-center gap-2 text-red-600'>
-                <RiDeleteBin5Line 
+                <RiDeleteBin5Line onClick={onDelete}
                 className='text-[20px] cursor-pointer hover:text-red-600'
                 />
             </div>
