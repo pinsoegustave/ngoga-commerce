4f0910a99a8a4cbf2df709796e047cf73b9c68be
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-10-14 15:56:31 +0200

    Blog Section: Added packages

--- components/admin-panel/AddBlogPopup.tsx
+++ components/admin-panel/AddBlogPopup.tsx
@@ -1,13 +1,41 @@
 "use client"
 import React, { Dispatch, SetStateAction } from 'react'
 import { IoIosCloseCircleOutline } from 'react-icons/io'
+import { Form, FormProvider, useForm } from 'react-hook-form'
+import * as z from "zod";
+import { FormControl, FormField, FormItem, FormLabel } from '../ui/form';
+import { Input } from '../ui/input';
+import { zodResolver } from "@hookform/resolvers/zod"
 
 interface PropsType {
     setOpenPopUp: Dispatch<SetStateAction<boolean>>;
 }
 
 const AddBlogPopup = ({ setOpenPopUp }: PropsType) => {
+    
+    const formSchema = z.object({
+        title: z
+            .string()
+            .min(5, { message: "Hey the title is not long enough" })
+            .max(100, { message: "It&apos;s too long " }),
+        price: z
+            .number().min(5, { message: "Hey the title is " }),
+        description: z
+            .string()
+            .min(5, { message: "Hey the title is not long enough" })
+            .max(100, { message: "It's too long" })
+            .trim()
+    })
 
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        mode: "onChange",
+        defaultValues: {
+            title: "",
+            price: 29.09,
+            description: "",
+        },
+    })
   return (
     <div className='fixed top-0 left-0 w-full h-screen bg-[#00000070] grid place-items-center'>
         <div className='bg-white w-[700px] py-8 rounded-lg text-center relative'>
@@ -17,7 +45,25 @@ const AddBlogPopup = ({ setOpenPopUp }: PropsType) => {
             />
 
             <h2 className='text-2xl'>Add a new blog</h2>
-            <form className="mt-6 w-fit space-y-4 mx-auto">
+            <FormProvider {...form}>
+                <form>
+                    <FormField
+                        control={form.control}
+                        name= "title"
+                        render={({ field }) => (
+                            field.value,
+                            <FormItem>
+                                <FormLabel>Title</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Main title for your blog" />
+                                </FormControl>
+                            </FormItem>
+                        )}
+                        />
+                </form>
+            </FormProvider>
+
+            {/* <form className="mt-6 w-fit space-y-4 mx-auto">
                 <input type="text"
                 className='border block border-gray-500 outline-none px-4 py-2 rounded-lg w-fit' 
                 placeholder='Blog Image'
@@ -38,7 +84,7 @@ const AddBlogPopup = ({ setOpenPopUp }: PropsType) => {
                         Save blog
                     </button>
                 </div>
-            </form>
+            </form> */}
         </div>
     </div>
   )
