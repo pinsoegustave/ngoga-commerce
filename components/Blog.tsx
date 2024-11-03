07fa8eac31c598a7358e0fd731f9cdf12f12f871
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-11-03 12:40:36 +0200

    Frontend: Blogs fetch on homepage

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -1,19 +1,33 @@
-import { ChatBubbleLeftIcon, ChatBubbleLeftRightIcon, UserCircleIcon } from '@heroicons/react/16/solid'
+import { ChatBubbleLeftRightIcon, UserCircleIcon } from '@heroicons/react/16/solid'
+import { GetServerSideProps, NextPage } from 'next'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Blog = () => {
+type Blog = {
+    _id: string;
+    blogImage: string;
+    blogTitle: string;
+    description: string;
+}
+
+type BlogProps = {
+    blogs: Blog[];
+}
+
+const Blog: NextPage<BlogProps> = ({ blogs }) => {
+
   return (
     <div className='pt-[4rem] md:pt-[8rem] pb-[4rem] bg-[#02050a]'>
         <h1 className='heading'>
             MY <span className='text-yellow-400'>BLOG</span>
         </h1>
         <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-[4rem] gap-[3rem] w-[80%] mx-auto'>
-            <div>
+           { blogs && blogs.map((blog) => ( 
+            <div key={blog._id}>
                 <div className='w-[100%] relative h-[400px]'>
                     <Image 
-                    src={'/suitman.webp'}
-                    alt='blog1'
+                    src={blog.blogImage}
+                    alt={blog.blogTitle}
                     layout='fill'
                     className='object-cover'
                     />
@@ -32,10 +46,11 @@ const Blog = () => {
                             <p className='text-white'>Comments(4)</p>
                         </div>
                     </div>
-                    <p className='mt-[1rem] text-white text-[18px] font-semibold'>Industrial Design and Impementation</p>
+                    <p className='mt-[1rem] text-white text-[18px] font-semibold'>{blog.blogTitle}</p>
                 </div>
             </div>
-            <div>
+            ))}
+            {/* <div>
                 <div className='w-[100%] relative h-[400px]'>
                     <Image 
                     src={'/suitman.webp'}
@@ -60,8 +75,8 @@ const Blog = () => {
                     </div>
                     <p className='mt-[1rem] text-white text-[18px] font-semibold'>My journey in music</p>
                 </div>
-            </div>
-            <div>
+            </div> */}
+            {/* <div>
                 <div className='w-[100%] relative h-[400px]'>
                     <Image 
                     src={'/theman.webp'}
@@ -86,10 +101,31 @@ const Blog = () => {
                     </div>
                     <p className='mt-[1rem] text-white text-[18px] font-semibold'>Best practices for a mathematician</p>
                 </div>
-            </div>
+            </div> */}
         </div>
     </div>
   )
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    try {
+        const res = await fetch('/api/get_blogs');
+        const data = await res.json();
+
+        return {
+            props: {
+                blogs: data.blogs || [],
+            },
+        };
+    } catch (error) {
+        console.error('Error fetching blogs data:', error);
+    }
+    // Fallback data incase of an error
+    return {
+        props: {
+            blogs: [],
+        }
+    }
 }
 
-export default Blog
+export default Blog;
