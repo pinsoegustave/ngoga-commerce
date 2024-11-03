bdb61227cbfadc35aed8adf113c5d0d683c5a2f0
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-11-03 09:22:18 +0200

    Fetch books to the frontend

--- components/Books.tsx
+++ components/Books.tsx
@@ -1,27 +1,50 @@
+import axios from 'axios';
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+interface IBooks {
+    _id: string;
+    book_imgSrc: string;
+    book_name: string;
+    book_URL: string;
+}
 
 const Books = () => {
+
+    const [ books, setBooks ] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/get_books')
+            .then((res) => {
+                console.log(res.data)
+                setBooks(res.data);
+            })
+            .catch((err) => console.error(err));
+    }, []);
+    
   return (
     <div className='bg-[#02050a] pt-[4rem] md:pt-[8rem] pb-[1rem]'>
         <h1 className='heading'>MY BO
             <span className='text-yellow-400'>OKS</span>
         </h1>
         <div className='w-[80%] pt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem]'>   
-            <Link href={'https://drive.google.com/file/d/1j4BGiud9TA0C_Pi9NYVoJTtXDnftbADX/preview'} target='_blank'>
+        { books.map((booking: IBooks) => (
+            <Link key={booking._id} href={booking.book_URL} target='_blank'>
                 <div data-aos="fade-up">
                     <div className='transform cursor-pointer hover:-translate-y-6 transition-all druration-200 relative w-[100%] h-[200px] md:h-[300px]'>
-                        <Image 
-                        src={'/book2.webp'}
-                        alt='book1'
-                        layout='fill'
-                        className='object-contain'
-                        />
+                            <Image
+                            src={booking.book_imgSrc}
+                            alt={booking.book_name}
+                            layout='fill'
+                            className='object-contain'
+                            />
                     </div>
                 </div>
             </Link>
-            <div data-aos="fade-up" data-aos-delay="300">
+        ))}
+        </div>
+            {/* <div data-aos="fade-up" data-aos-delay="300">
                 <div className='transform cursor-pointer hover:-translate-y-6 transition-all druration-200 relative w-[100%] h-[200px] md:h-[300px]'>
                     <Image 
                     src={'/book4.jfif'}
@@ -71,10 +94,8 @@ const Books = () => {
                     />
                     
                 </div>
-            </div>
-            <Link href="https://drive.google.com/file/d/1j4BGiud9TA0C_Pi9NYVoJTtXDnftbADX/preview" className='text-white bg-teal-600 m-4 p-4' target='_blank'> Click here to open the book
-            </Link>
-        </div>
+            </div> */}
+        {/* // </div> */}
     </div>
   )
 }
