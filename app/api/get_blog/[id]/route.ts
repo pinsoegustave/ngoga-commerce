5e806b7bb7da3335d4ff421552a3fd1dc6132641
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-11-10 14:53:13 +0200

    Create get blog by Id API

--- app/api/get_blog/[id]/route.ts
+++ app/api/get_blog/[id]/route.ts
@@ -1,1 +1,25 @@
+import Blog from "@/libs/models/Blog";
+import { connectMongoDB } from "@/libs/MongoConnect";
+import { NextRequest, NextResponse } from "next/server";
 
+
+export async function GET(request: NextRequest, URLParams: any) {
+    try {
+        const id = URLParams.params.id;
+
+        await connectMongoDB();
+        const data = await Blog.findById(id);
+
+        return NextResponse.json({
+            msg: "get your data",
+            data
+        });
+    } catch (err) {
+        return NextResponse.json({
+            err,
+            msg: "Something went wrong"
+        }, {
+          status: 400  
+        });
+    }
+}
